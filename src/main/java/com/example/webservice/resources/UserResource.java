d7d0f0624375265d87b1d28d080688c65ca2c1d9
Warley Dev <warleycm7@gmail.com>
2023-04-19 17:13:10 -0300

    Implementação da camada de serviço, e fazendo o registro dos componentes

--- src/main/java/com/example/webservice/resources/UserResource.java
+++ src/main/java/com/example/webservice/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.example.webservice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webservice.entities.User;
+import com.example.webservice.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping	
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Victoria", "choivivih0203@gmail.com", "999429568", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id ){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+	
 }
