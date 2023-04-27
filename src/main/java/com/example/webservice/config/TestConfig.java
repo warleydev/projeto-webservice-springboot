93d3aa7603b2824b532b40d9c0931772eba07f0c
Warley Dev <warleycm7@gmail.com>
2023-04-27 20:16:13 -0300

    Criação da entidade Payment e associação one-to-one entre ela e a entidade Order

--- src/main/java/com/example/webservice/config/TestConfig.java
+++ src/main/java/com/example/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.webservice.entities.Category;
 import com.example.webservice.entities.Order;
 import com.example.webservice.entities.OrderItem;
+import com.example.webservice.entities.Payment;
 import com.example.webservice.entities.Product;
 import com.example.webservice.entities.User;
 import com.example.webservice.entities.enums.OrderStatus;
@@ -80,7 +81,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
-
-	
 }
