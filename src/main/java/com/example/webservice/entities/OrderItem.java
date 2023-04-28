c5e4d9fe7c3c3d7f4e9387917c58b757cc6c14f5
Warley Dev <warleycm7@gmail.com>
2023-04-28 17:54:31 -0300

    Implementação dos métodos subTotal (OrderItem) e Total(Order)

--- src/main/java/com/example/webservice/entities/OrderItem.java
+++ src/main/java/com/example/webservice/entities/OrderItem.java
@@ -66,6 +66,10 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 
+	public Double getSubTotal(){
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -82,4 +86,6 @@ public class OrderItem implements Serializable{
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
+	
+	
 }
